7628840ffac8b83bd42be433684fe4cc72310ea4
Ghaieth Kahel <ghaiethka@gmail.com>
2026-01-08 09:46:15 +0300

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    /////////////////////// REGISTER
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        // ✅ إنشاء Profile تلقائيًا
+        
+
+
+        // إنشاء توكن
+        $token = $user->createToken('api_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'تم التسجيل بنجاح',
+            'token' => $token,
+        ], 201);
+    }
+
+    /////////////////////// LOGIN
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'البيانات غير صحيحة'
+            ], 401);
+        }
+
+        // حذف التوكنات القديمة (اختياري)
+        $user->tokens()->delete();
+
+        // إنشاء توكن جديد
+        $token = $user->createToken('api_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'تم تسجيل الدخول بنجاح',
+            'token' => $token,
+        ]);
+    }
+
+    /////////////////////// LOGOUT
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'تم تسجيل الخروج بنجاح'
+        ]);
+    }
+}
